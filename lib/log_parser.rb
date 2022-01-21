7352ed365ac29406f01790c1caec4d251188d67b
Matheus_Lacerda <matheus_mtl@hotmail.com>
2022-01-21 16:25:32 -0300

    fixes

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -21,24 +21,24 @@ class LogParser
     }
     end
 
-      private
+  private
 
-        def lines_counter
-          File.readlines(@file_path).count
-        end
+  def lines_counter
+    File.readlines(@file_path).count
+  end
 
-        def players
-          game_players = []
-          File.readlines(@file_path).each do |line|
-            if line.include? 'killed' then
-              game_players << line.split('killed')[0].split(':').last.strip
-              game_players << line.split('killed')[1].split('by').first.strip
-            end
-          end
-          game_players = game_players.uniq
-          if game_players.include? '<world>' then
-            game_players.delete_at(game_players.index('<world>'))
-          end
-            game_players
-        end
+  def players
+    game_players = []
+    File.readlines(@file_path).each do |line|
+      if line.include? 'killed' then
+        game_players << line.split('killed')[0].split(':').last.strip
+        game_players << line.split('killed')[1].split('by').first.strip
+      end
+    end
+    game_players = game_players.uniq
+    if game_players.include? '<world>' then
+      game_players.delete_at(game_players.index('<world>'))
+    end
+    game_players
+  end
 end
