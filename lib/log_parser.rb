7195c5ec1f26d15ab739b7746ab19ddd736c5a31
Matheus_Lacerda <matheus_mtl@hotmail.com>
2022-01-25 10:57:31 -0300

    simplecov test coveres

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -30,15 +30,13 @@ class LogParser
   def players
     game_players = []
     File.readlines(@file_path).each do |line|
-      if line.include? 'killed' then
+      if line.include? 'killed'
         game_players << line.split('killed')[0].split(':').last.strip
         game_players << line.split('killed')[1].split('by').first.strip
       end
     end
     game_players = game_players.uniq
-    if game_players.include? '<world>' then
-      game_players.delete_at(game_players.index('<world>'))
-    end
+    game_players.delete_at(game_players.index('<world>'))
     game_players
   end
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative '../lib/log_parser'
 require_relative 'spec_helper'
+require_relative '../lib/log_parser'
 
 describe LogParser do
   describe '#initialize' do
