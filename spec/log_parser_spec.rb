7fd882d5ae6eebdcfe03552bfcc71c510ddbfe7d
Matheus_Lacerda <matheus_mtl@hotmail.com>
2022-01-28 10:49:59 -0300

    task 5 teste

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -5,7 +5,7 @@ json_object = {
     "./spec/fixtures/test_file.log": {
     "lines": 10,
     "players": ["Isgalamido", "Mocinha", "Dono da Bola", "Zeh"],
-    "kills": {"Dono da Bola"=> 1, "Isgalamido"=> 2},
+    "kills": {"Dono da Bola"=> 1, "Isgalamido"=> -1},
     "total_kills": 3
     }
   }
